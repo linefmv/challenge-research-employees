43cf42e1bc7ed84f3ec0baf358f3bacda53f636f
linefmv <alinemaciel018@gmail.com>
2022-05-07 16:27:29 -0300

    feat: fit style

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,13 +1,17 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Container = styled.main`
-  width: 250px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
+export const Container = styled.header`
+  width: 100%;
+  height: 100px;
+  padding: 5px 0 0 5px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 2fr;
+  align-items: center;
+
+  @media (max-width: 750px) {
+    grid-template-columns: 1fr;
+  }
+`;
 export const Image = styled.img`
   width: 150px;
-  align-self: center;
-  border-radius: 2vh;
-`
+`;
