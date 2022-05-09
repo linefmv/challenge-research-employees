de36887ba74a2f6ffc646de5c6ee6d37e463e2c3
linefmv <alinemaciel018@gmail.com>
2022-05-08 23:45:56 -0300

    feat: styling table

--- src/components/EmployeesList/styles.js
+++ src/components/EmployeesList/styles.js
@@ -11,35 +11,47 @@ export const Container = styled.main`
   table {
     width: 95%;
     border-collapse: collapse;
-    border: 1px solid lime;
+    border: none;
   }
-  th {
-    border: 1px solid lime;
+
+   th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #5c5cff;
+    color: white;
+
+    &:first-child {
+      border-radius: 10px 0 0 10px;
+    }
+    &:last-child {
+      border-radius: 0 10px 10px 0;
+    }
   }
-  
-  `;
+`;
 
 export const EmployeeDetails = styled.tr`
-border-spacing: 0 150px;
-    border: 1px solid rgb(190, 190, 190);
-  text-align: center;
   & :nth-child(-n + 2) {
     width: fit-content;
     display: flex;
   }
+ text-align: center;
 `;
 export const EmployeeName = styled.td`
   grid-area: name;
+  border-radius: 10px 0 0 0;
 `;
 export const EmployeeRegister = styled.td`
   grid-area: register;
 `;
 export const EmployeePosition = styled.td`
   grid-area: position;
+  border-radius: 0 0 0 10px;
 `;
 export const EmployeeBranch = styled.td`
   grid-area: branch;
 `;
 export const EmployeeAdmission = styled.td`
   grid-area: admission;
+  border-radius: 0 10px 10px 0;
 `;
