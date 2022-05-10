a40088b34262ad3dc1247be21f523af62fb3f911
linefmv <alinemaciel018@gmail.com>
2022-05-09 23:00:27 -0300

    style: identation

--- src/components/IconComponent/Icon.jsx
+++ src/components/IconComponent/Icon.jsx
@@ -2,15 +2,14 @@ import { Button, Image } from "./styles";
 
 const IconComponent = ({ size, className, style, onClick, src }) => {
   return (
-    <Button onClick={onClick}> 
-    <Image
-      className={className}
-      style={style}
-      width={`${size}px`}
-      height={`${size}px`}
-      src={src}
-      
-    />
+    <Button onClick={onClick}>
+      <Image
+        className={className}
+        style={style}
+        width={`${size}px`}
+        height={`${size}px`}
+        src={src}
+      />
     </Button>
   );
 };
