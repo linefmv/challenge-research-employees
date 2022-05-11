5046dceb475ca34be4d6a790408bf5b94dcd467f
linefmv <alinemaciel018@gmail.com>
2022-05-11 02:53:55 -0300

    feat: passing data to employees

--- src/components/EmployeesList/EmployeesList.jsx
+++ src/components/EmployeesList/EmployeesList.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import getData from "../../service/api";
-import IconComponent from "../IconComponent/Icon";
 import Header from "../Header/Header";
 import TableRow from "../TableRow/TableRow";
 
 import { Container } from "./styles";
+import TableHead from "../TableHead/TableHead";
 
 const EmployeesList = ({ employeesItems }) => {
   const [data, setData] = useState([]);
@@ -19,11 +19,6 @@ const EmployeesList = ({ employeesItems }) => {
     fetchData();
   }, []);
 
-  const handleSortNamebyAsc = () => {
-    const sortedData = [...data].sort((a, b) => a.nome.localeCompare(b.nome));
-    setData(sortedData);
-  };
-
   return (
     <Container>
       <Header
@@ -32,24 +27,11 @@ const EmployeesList = ({ employeesItems }) => {
         setData={setData}
       />
       <table>
-        <thead>
-          <tr>
-            {employeesItems.map((item, index) => {
-              return (
-                <th key={index} scope="col">
-                  {item}
-                  <IconComponent
-                    onClick={handleSortNamebyAsc}
-                    className="caretImg caret"
-                    rel="icon"
-                    src="https://img.icons8.com/external-royyan-wijaya-detailed-outline-royyan-wijaya/24/000000/external-arrow-down-arrow-line-royyan-wijaya-detailed-outline-royyan-wijaya.png"
-                    alt="Icon with arrow down"
-                  />
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
+        <TableHead
+          employeesItems={employeesItems}
+          setData={setData}
+          data={data}
+        />
         <tbody>
           <TableRow data={data} setData={setData} />
         </tbody>
