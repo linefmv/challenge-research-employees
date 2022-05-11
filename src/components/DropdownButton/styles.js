43808bbddf1bdb1873c3b1201c40281859f6823e
linefmv <alinemaciel018@gmail.com>
2022-05-11 00:16:35 -0300

    feat: responsive header

--- src/components/DropdownButton/styles.js
+++ src/components/DropdownButton/styles.js
@@ -6,11 +6,24 @@ export const Container = styled.div`
   flex-direction: column;
 
   select {
-    height: 30px;
+    height: 40px;
     font-size: 15px;
+    background-color: white;
+    border: thin solid grey;
+    border-radius: 4px;
+    line-height: 1.5em;
+    padding: 0.5em 3.5em 0.5em 1em;
+    cursor: pointer;
+    box-shadow: 0 0 2px rgb(204, 204, 204);
+    transition: all 0.5s ease;
   }
 
   option {
     font-size: 15px;
+    color: #000;
+
+    :hover {
+      background-color: blue;
+    }
   }
 `;
