0aa569bf8c173789d8befe5ac1e6a5fb996e828e
linefmv <alinemaciel018@gmail.com>
2022-05-11 13:33:00 -0300

    feat: create back to top

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import getData from "./service/api";
 import EmployeesList from "./components/EmployeesList/EmployeesList";
+import Footer from "./components/Footer/Footer";
+import BackToTopButton from "./components/BackToTopButton/BackToTopButton";
+
 import "./App.css";
-import { Footer } from "./components/Footer/styles";
 
 function App() {
   const [data, setData] = useState([]);
@@ -18,7 +20,8 @@ function App() {
   return (
     <>
       <EmployeesList employeesItems={data} />
-      <Footer/>
+      <BackToTopButton />
+      <Footer />
     </>
   );
 }
