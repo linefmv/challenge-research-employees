e71845162de2a0504e3f3398ce686b999bb607e6
linefmv <alinemaciel018@gmail.com>
2022-05-11 21:17:14 -0300

    feat: separate component

--- src/components/EmployeesList/EmployeesList.jsx
+++ src/components/EmployeesList/EmployeesList.jsx
@@ -1,24 +1,14 @@
 import React, { useEffect, useState } from "react";
 import getData from "../../service/api";
-import SearchBar from "../SearchBar/SearchBar";
-import DropdownButton from "../DropdownButton/DropdownButton";
-import IconComponent from "../IconComponent/IconComponent";
+import Header from "../Header/Header";
+import TableRow from "../TableRow/TableRow";
 
-import {
-  Container,
-  EmployeeDetails,
-  EmployeeName,
-  EmployeeRegister,
-  EmployeePosition,
-  EmployeeBranch,
-  EmployeeAdmission,
-} from "./styles";
+import { Container } from "./styles";
+import TableHead from "../TableHead/TableHead";
 
 const EmployeesList = ({ employeesItems }) => {
   const [data, setData] = useState([]);
-  const [searchApiData, setSearchApiData] = useState([]);
-  const [filterValue, setFilterValue] = useState("");
-  const [getValueInsideOption, setOptionValue] = useState("");
+  const [searchData, setSearchData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,47 +19,6 @@ const EmployeesList = ({ employeesItems }) => {
     fetchData();
   }, []);
 
-<<<<<<< HEAD
-=======
-  const handleFilter = (event) => {
-    if (event.target.value === "") {
-      setData(searchApiData);
-    }
-    if (getValueInsideOption === "Funcionário(a)") {
-      const filterConfig = searchApiData.filter((item) =>
-        item.nome.toLowerCase().includes(event.target.value.toLowerCase())
-      );
-      setData(filterConfig);
-    }
-    if (getValueInsideOption === "Filial") {
-      const filterConfig = searchApiData.filter((item) =>
-        item.filial.toLowerCase().includes(event.target.value.toLowerCase())
-      );
-      setData(filterConfig);
-    }
-    if (getValueInsideOption === "Matricula") {
-      const filterConfig = searchApiData.filter((item) =>
-        item.matricula.toLowerCase().includes(event.target.value.toLowerCase())
-      );
-      setData(filterConfig);
-    }
-    setFilterValue(event.target.value);
-  };
-
-  const handleSortNamebyAsc = () => {
-    const sortedData = [...data].sort((a, b) => a.nome.localeCompare(b.nome));
-    setData(sortedData);
-  };
-
-  const handleRemoveRow = (rowId) => {
-    const newData = [...data];
-    const index = data.findIndex((data) => data.id === rowId);
-
-    newData.splice(index, 1);
-    setData(newData);
-  };
-
->>>>>>> development
   return (
     <>
       <Container>
