1be50c52b2429d44d47929a21f5198de869f6584
linefmv <alinemaciel018@gmail.com>
2022-07-22 00:31:43 -0300

    feat: set root in index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Root from './pages/Root';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
-
