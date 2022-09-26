895cdec004a8db7974107de28672839f9f84af02
linefmv <alinemaciel018@gmail.com>
2022-09-25 21:17:02 -0300

    test: refactor test

--- cypress/e2e/user-flow.cy.js
+++ cypress/e2e/user-flow.cy.js
@@ -1,29 +1,38 @@
+
 describe('login test', () => {
-  it('acess login', () => {
-    cy.visit('find-your-employees.vercel.app/login')
-    cy.get('#user').type('admin')
-    cy.get('#password').type('admin')
-    cy.get('[type="submit"]').click()
+  context('720p resolution', () => {
+    beforeEach(() => {
+      // run these tests as if in a desktop
+      // browser with a 720p monitor
+      cy.viewport(1280, 720)
+    })
+
+    it('acess login', () => {
+      cy.visit('find-your-employees.vercel.app/login')
+      cy.get('#user').type('admin')
+      cy.get('#password').type('admin')
+      cy.get('[type="submit"]').click()
+    })
   })
-})
 
-describe('Testing funcionalities', () => {
-  it('using filters and find employee', () => {
-    cy.get('select').select("Funcionário(a)")
-    cy.get('input').type('Roman')
-  })
+  describe('Testing funcionalities', () => {
+    it('using filters and find employee', () => {
+      cy.get('select').select("Funcionário(a)")
+      cy.get('input').type('Roman')
+    })
 
-  it("Remove Roman of table", () => {
-    cy.get('.deleteIcon').click()
-  }); 
+    it("Remove Roman of table", () => {
+      cy.get('.deleteIcon').click()
+    });
 
-  it("Remove Roman of input and Scroll down view and click in button 'Back to top'", () => {
-    cy.get('input').clear()
-    cy.scrollTo(0, 1000)
-    cy.get('.active').click()
-  }); 
+    it("Remove Roman of input and Scroll down view and click in button 'Back to top'", () => {
+      cy.get('input').clear()
+      cy.scrollTo(0, 1000)
+      cy.get('.active').click()
+    });
 
-  it("Click on sort data, to see employees order to A-Z", () => {
-    cy.get('[alt="Icon with arrow"]').click()
-  })
-})
+    it("Click on sort data, to see employees order to A-Z", () => {
+      cy.get('.caretIcon').first().click()
+    })
+  });
+});
